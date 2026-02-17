090a4fce4c5bf7957f5ec899d1441deadf1bc422
UncleBenon <burningvenom@gmail.com>
2026-02-17 12:45:44 +0100

    progress, the database seeds correctly now

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,15 +5,54 @@ import reviewData from "../src/data/reviews.json" with { type: 'json' };
 import bookingData from "../src/data/bookings.json" with { type: 'json' };
 import propertyData from "../src/data/properties.json" with { type: 'json' };
 
-// TODO:
-//      Seed all the data correctly to the database.
+// No idea what to do with the amenities.json:
+// Nowhere in the instructions is it explained, nor can i cross-ref the ids with any.
 
 async function main() {
-    console.log(`Found ${userData.users.length} users.`);
-    console.log(`Found ${hostData.hosts.length} hosts.`);
-    console.log(`Found ${reviewData.reviews.length} reviews.`);
-    console.log(`Found ${bookingData.bookings.length} bookings.`);
-    console.log(`Found ${propertyData.properties.length} properties.`);
+    console.log(`Found ${userData.users.length} users, Seeding.`);
+    for (const user of userData.users) {
+        await prisma.user.upsert({
+            where: { id: user.id },
+            update: {},
+            create: user
+        });
+    }
+
+    console.log(`Found ${hostData.hosts.length} host, Seedings.`);
+    for (const host of hostData.hosts) {
+        await prisma.host.upsert({
+            where: { id: host.id },
+            update: {},
+            create: host
+        });
+    }
+
+    console.log(`Found ${reviewData.reviews.length} review, Seedings.`);
+    for (const review of reviewData.reviews) {
+        await prisma.review.upsert({
+            where: { id: review.id },
+            update: {},
+            create: review
+        });
+    }
+
+    console.log(`Found ${bookingData.bookings.length} booking, Seedings.`);
+    for (const booking of bookingData.bookings) {
+        await prisma.booking.upsert({
+            where: { id: booking.id },
+            update: {},
+            create: booking
+        });
+    }
+
+    console.log(`Found ${propertyData.properties.length} properties, Seedings.`);
+    for (const property of propertyData.properties) {
+        await prisma.property.upsert({
+            where: { id: property.id },
+            update: {},
+            create: property
+        });
+    }
 }
 
 main()
