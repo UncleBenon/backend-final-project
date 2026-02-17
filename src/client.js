07a8d81c9218b085eab8fc86b8629233abb0a346
UncleBenon <burningvenom@gmail.com>
2026-02-17 20:54:58 +0100

    progress, Setting up for the other ends.

--- src/client.js
+++ src/client.js
@@ -1,8 +1,9 @@
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from './generated/prisma/client.js';
+import "dotenv/config";
 
 const adapter = new PrismaBetterSqlite3({
-    url: "file:./prisma/dev.db",
+    url: process.env["DATABASE_URL"],
     log: ['query', 'info', 'warn', 'error']
 })
 
