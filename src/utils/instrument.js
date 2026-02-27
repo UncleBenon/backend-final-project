f5660ed6e2dabf56cab545bd5e99363367e0198c
UncleBenon <burningvenom@gmail.com>
2026-02-27 17:04:12 +0100

    oops.

--- src/utils/instrument.js
+++ src/utils/instrument.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import * as Sentry from "@sentry/node";
 
 Sentry.init({
-    dsn: "https://fb9014cefd62dd32c0258aa1c6045b49@o4510957599457280.ingest.de.sentry.io/4510957676003408",
+    dsn: process.env.SENTRY_DNS,
     enableLogs: true,
     sendDefaultPii: true,
 });
